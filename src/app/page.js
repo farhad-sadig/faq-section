39993e75937cf60df67940ff5ef075bb0e8c9a07
Farhad Sadig <fsadigza@gmail.com>
2024-06-15 16:15:29 -0700

    Remove excess Tailwind classnames

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import FAQsections from "@/FAQsections";
 
 export default function Home() {
 	return (
-		<div className="flex bg-gradient-to-b from-gray-50 to-[#d2d6db] p-4">
-			<div className="flex flex-col bg-white rounded gap-7 p-4 py-12 tablet:rounded-md tablet:py-16 tablet:gap-16 desktop:p-24">
+		<div className="flex bg-gradient-to-b from-gray-50 to-gray-200 p-4">
+			<div className="flex flex-col bg-white rounded gap-7 p-4 tablet:py-16 tablet:gap-16 desktop:p-24">
 				<h1 className="font-semibold text-3xl text-center text-neutral-900 tablet:text-5xl">
 					Frequently asked questions
 				</h1>
-				<div className="font-normal text-lg text-center text-neutral-600 mb-2 tablet:text-xl">
+				<div className="text-lg text-center text-neutral-600 mb-2 tablet:text-xl">
 					Choose any questions you need
 				</div>
 
@@ -23,17 +23,17 @@ export default function Home() {
 								id={index}
 							/>
 							{index < FAQsections.length - 1 && (
-								<hr className="flex h-px bg-neutral-300 self-stretch" />
+								<hr className="h-px bg-neutral-300 w-full" />
 							)}
 						</>
 					))}
 				</div>
-				<div className="flex flex-col justify-center gap-4 self-stretch p-4 rounded-lg border border-solid border-neutral-200 tablet:flex-row tablet:justify-between tablet:p-8">
-					<div className="flex flex-col justify-center gap-2">
+				<div className="flex flex-col gap-4 p-4 rounded-lg border border-neutral-200 tablet:flex-row tablet:justify-between tablet:p-8">
+					<div className="flex flex-col gap-2">
 						<span className="font-semibold text-xl text-neutral-900">
 							Can’t find the answer you’re looking for?
 						</span>
-						<span className="font-normal text-base">
+						<span className="text-base">
 							Reach out to our&nbsp;
 							<span className="text-indigo-700">
 								<a href="/">customer support&nbsp;</a>
@@ -42,7 +42,7 @@ export default function Home() {
 						</span>
 					</div>
 
-					<button className="btn-indigo flex justify-center items-center self-stretch font-normal text-base text-white bg-indigo-700 px-5 py-3 rounded hover:bg-indigo-800 focus:bg-indigo-800">
+					<button className="btn-indigo flex justify-center items-center font-normal text-base text-white bg-indigo-700 px-5 py-3 rounded hover:bg-indigo-800 focus:bg-indigo-800">
 						Get in touch
 					</button>
 				</div>
--- src/components/AccordionSection.jsx
+++ src/components/AccordionSection.jsx
@@ -11,12 +11,12 @@ const AccordionSection = ({ title, content, id }) => {
 	};
 
 	return (
-		<div className="flex gap-4 rounded-xl">
-			<div className="flex flex-col gap-2">
+		<div className="flex gap-4 rounded-xl w-full">
+			<div className="flex flex-col w-full gap-2">
 				<button
 					type="button"
 					role="button"
-					className="flex self-stretch justify-between"
+					className="flex justify-between w-full"
 					onClick={toggleSection}
 					aria-expanded={isOpen.toString()}
 					aria-controls={`faq-${id}`}
@@ -26,9 +26,7 @@ const AccordionSection = ({ title, content, id }) => {
 				</button>
 
 				<p
-					className={`font-normal text-base text-neutral-600 ${
-						isOpen ? "" : "hidden"
-					}`}
+					className={`text-base text-neutral-600 ${isOpen ? "" : "hidden"}`}
 					id={`faq-${id}`}
 				>
 					{content}
