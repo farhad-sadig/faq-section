39d7606623add8ccec7b2164e6f9415bd249f21f
Farhad Sadig <fsadigza@gmail.com>
2024-06-15 14:53:53 -0700

    Initial commit

---
+++ src/components/AccordionSection.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+import ExpandIcon from "./ExpandIcon";
+import CollapseIcon from "./CollapseIcon";
+
+const AccordionSection = ({ title, content, id }) => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	const toggleSection = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<div className="flex gap-4 self-stretch rounded-xl">
+			<div className="flex flex-col justify-center gap-2 grow">
+				<button
+					type="button"
+					role="button"
+					className="flex self-stretch justify-between"
+					onClick={toggleSection}
+					aria-expanded={isOpen.toString()}
+					aria-controls={`faq-${id}`}
+				>
+					<span className="font-medium text-lg text-neutral-900">{title}</span>
+					{isOpen ? <CollapseIcon /> : <ExpandIcon />}
+				</button>
+
+				<p
+					className={`font-normal text-base text-neutral-600 ${
+						isOpen ? "" : "hidden"
+					}`}
+					id={`faq-${id}`}
+				>
+					{content}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default AccordionSection;
